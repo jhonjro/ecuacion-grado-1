59249c8979f2679d3101cdc21bc97e3f2cd001c9
tenma18 <jhonjairorojasortiz@gmail.com>
2022-01-23 15:04:36 -0500

    shortcut icon

--- test_code_game.js
+++ test_code_game.js
@@ -107,8 +107,6 @@ document.addEventListener('DOMContentLoaded', function (event) {
         return resultado;
     };
     resolver.addEventListener('click', function () {
-        console.log(resolver.innerText.indexOf('Click aquí'));
-        console.log(resolver.innerHTML);
         if (resolver.innerText.indexOf('Click aquí') != -1) {
             alert("Sintaxis:\n    n\u00FAmero operador n\u00FAmero = n\u00FAmero operador n\u00FAmero,\ndonde uno de esos n\u00FAmeros debe de ser 'x' y los dem\u00E1s n\u00FAmeros enteros.\nNota:\n    Los operadores aceptados son los siguientes :  *, /, +, -\nEjemplo:\n    34534+56756 = 45645/x");
         }
